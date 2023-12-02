2a214f3c6ad03a47eea373e9bbf78dbd99d68383
Siva Kartheek <67451273+SivaKartheekVeeraboina@users.noreply.github.com>
2023-12-02 21:55:32 +0530

    Update jfrog.py

--- jfrog.py
+++ jfrog.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python3
+
 import requests
+import subprocess
 
-def jfrogUpload():
-    url = "http://54.144.129.71:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
-    file_path = '/home/ubuntu/Java_app_3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
+def jfrogUpload() :
+    # define the url file path, and authentication credentials and change ur IP Address
+    url = 'http://54.144.129.71:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
+    file_path = '/home/ubuntu/java-3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
     username = 'admin'
-    password = 'Admin@123'
+    password = 'Admin@123' #replace 'your password with ur actual password'
 
-    with open(file_path,'rb') as file:
+    #send the PUT request with authentication and file upload
+    with open(file_path, 'rb') as file:
         response = requests.put(url, auth=(username, password), data=file)
+    #check the response.status code
     if response.status_code == 201:
         print("\nPUT request was successful!")
     else:
-        print(f"PUT request failed with status code(response.status_code)")
-        print("Response content:")
+        print("PUT request failed with status code (response.status_code)")
+        print("response content:")
         print(response.text)
+def mvnBuild() :
+    #define the Maven Command
+    maven_command = "mvn clean install -DskipTests"
 
+    #Run the maven command as a subprocess
+    try:
+        subprocess.run(maven_command, check=True, text=True, shell=True)
+        print("/nMaven build completed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error: Maven Build failed with exit code (e.returncode)")
 
-def main():
- 
+def main() :
+    #mvnBuild()
     jfrogUpload()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
