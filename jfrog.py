0511dbfa3dcbc01cb0ce501133185c9f71512297
Siva Kartheek <67451273+SivaKartheekVeeraboina@users.noreply.github.com>
2023-12-03 19:33:51 +0530

    Update jfrog.py

--- jfrog.py
+++ jfrog.py
@@ -5,10 +5,10 @@ import subprocess
 
 def jfrogUpload() :
     # define the url file path, and authentication credentials and change ur IP Address
-    url = 'http://54.144.129.71:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
-    file_path = '/home/ubuntu/java-3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
+    url = 'http://52.86.169.160:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
+    file_path = '/home/ubuntu/Test_Assignment/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
     username = 'admin'
-    password = 'Admin@123' #replace 'your password with ur actual password'
+    password = 'password' #replace 'your password with ur actual password'
 
     #send the PUT request with authentication and file upload
     with open(file_path, 'rb') as file:
